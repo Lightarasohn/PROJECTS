a6afd1d96f24a73453c7d55a5476c96a563fdd8b
Onur Demir <onurdemir1771@gmail.com>
2025-02-16 22:22:43 +0300

    Yeni Projeler

---
+++ Making Backend To Free Website Template/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Making_Backend_To_Free_Website_Template.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
