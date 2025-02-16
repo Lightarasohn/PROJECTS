c8172f333f6b535044cde41bf292f47ccf7d2d62
Onur Demir <onurdemir1771@gmail.com>
2025-02-17 00:49:17 +0300

    Stock Controller methodları oluşturuldu

--- WebAPITutorial/api/Program.cs
+++ WebAPITutorial/api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,5 +25,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
