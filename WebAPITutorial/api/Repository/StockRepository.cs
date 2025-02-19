86f65bd06fff11e3decb19b3b93279e046e02b8d
Onur Demir <onurdemir1771@gmail.com>
2025-02-19 19:24:59 +0300

    Pagination (QuerySize)

--- WebAPITutorial/api/Repository/StockRepository.cs
+++ WebAPITutorial/api/Repository/StockRepository.cs
@@ -61,8 +61,9 @@ namespace api.Repository
                     : stocks.OrderBy(so => so.Symbol);
                 }
             }
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
 
-            return await stocks.ToListAsync();
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
