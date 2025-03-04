ac61a7ae4fd05e08ae69a9cccfdd13b017293c15
Onur Demir <onurdemir1771@gmail.com>
2025-03-04 22:44:51 +0300

    Contact oluşturma, Contact arama (id), Contact arama (email)

--- Making Backend To Free Website Template/Backend/Controller/ContactController.cs
+++ Making Backend To Free Website Template/Backend/Controller/ContactController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Backend.DTOs.Contact;
+using Backend.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controller
@@ -11,10 +12,25 @@ namespace Backend.Controller
     [ApiController]
     public class ContactController : ControllerBase
     {
+
+        private readonly IContactRepository _contactRepo;
+        public ContactController(IContactRepository contactRepo)
+        {
+            _contactRepo = contactRepo;
+        }
+
         [HttpPost]
         public async Task<IActionResult> CreateContact([FromBody] ContactCreateDto contact)
         {
-            return Ok();
+            if(!ModelState.IsValid)
+                return BadRequest("Body must be in the way it is");
+            
+            var createdContact = await _contactRepo.CreateContactAsync(contact);
+            
+            if(createdContact.IsEmailAlreadyExist)
+                return BadRequest("Email already has been contacted us");
+
+            return Ok(createdContact);
         }
     }
 }
--- Making Backend To Free Website Template/Backend/Interfaces/IContactRepository.cs
+++ Making Backend To Free Website Template/Backend/Interfaces/IContactRepository.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Backend.DTOs.Contact;
+using Backend.Models;
 
 namespace Backend.Interfaces
 {
     public interface IContactRepository
     {
-        public Task<ContactCreateDto> CreateContact(ContactCreateDto dto);
+        public Task<ContactCreatedDto> CreateContactAsync(ContactCreateDto dto);
+        public Task<Contact?> GetContactByIdAsync(int id);
+        public Task<Contact?> GetContactByEmailAsync(string email);
     }
 }
