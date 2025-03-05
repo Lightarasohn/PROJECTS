aef4736cf918a18a4a11251e3f1d524737715f0f
Onur Demir <onurdemir1771@gmail.com>
2025-03-05 18:07:47 +0300

    Contact modeline "Data Annotiations" eklendi

--- Making Backend To Free Website Template/Backend/DTOs/Contact/ContactCreateDto.cs
+++ Making Backend To Free Website Template/Backend/DTOs/Contact/ContactCreateDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,17 @@ namespace Backend.DTOs.Contact
 {
     public class ContactCreateDto
     {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(2)]
         public string? FullName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; } = string.Empty;
+        [MaxLength(64)]
         public string? Company { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(500)]
         public string? Message { get; set; } = string.Empty;
     }
 }
