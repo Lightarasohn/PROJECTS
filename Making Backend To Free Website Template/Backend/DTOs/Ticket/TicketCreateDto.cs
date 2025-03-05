f3ff15f1954df9232373e5b9414d3b3ee4afc9e6
Onur Demir <onurdemir1771@gmail.com>
2025-03-05 19:28:53 +0300

    Ticket modeline "Data Annotiations" eklendi

--- Making Backend To Free Website Template/Backend/DTOs/Ticket/TicketCreateDto.cs
+++ Making Backend To Free Website Template/Backend/DTOs/Ticket/TicketCreateDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Backend.Models;
@@ -8,11 +9,21 @@ namespace Backend.DTOs.Ticket
 {
     public class TicketCreateDto
     {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(4)]
         public string? FullName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; } = string.Empty;
+        [Required]
+        [Phone]
         public string? PhoneNumber { get; set; } = string.Empty;
+        [Required]
         public TicketType TicketType { get; set; }
+        [Required]
         public int NumberOfTickets { get; set; }
+        [MaxLength(255)]
         public string? AdditionalRequest { get; set; } = string.Empty;
     }
 }
