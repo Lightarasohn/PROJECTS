1307160c3dfe42d8e9fd7f82a3b31df6d3d9853a
Onur Demir <onurdemir1771@gmail.com>
2025-03-05 18:36:17 +0300

    Ticket oluşturma için DTO oluşturuldu ve Ticket Modeli düzenlendi. Mapper methodu eklendi.

--- Making Backend To Free Website Template/Backend/DTOs/Ticket/TicketCreateDto.cs
+++ Making Backend To Free Website Template/Backend/DTOs/Ticket/TicketCreateDto.cs
@@ -2,23 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.Forms;
+using Backend.Models;
 
-namespace Backend.Models
+namespace Backend.DTOs.Ticket
 {
-    public class Ticket
+    public class TicketCreateDto
     {
-        public int Id { get; set; }
         public string? FullName { get; set; } = string.Empty;
         public string? Email { get; set; } = string.Empty;
         public string? PhoneNumber { get; set; } = string.Empty;
         public TicketType TicketType { get; set; }
+        public int NumberOfTickets { get; set; }
         public string? AdditionalRequest { get; set; } = string.Empty;
-    }
-
-    public enum TicketType
-    {
-        EarlyBird = 1,
-        Standart = 2
     }
 }
