74cbd333b31b6b2a9eac5354587a6f0ef3174d97
Onur Demir <onurdemir1771@gmail.com>
2025-03-09 16:08:45 +0300

    Veri Tabanından bilgileri ve verileri çekme "dotnet ef dbcontext scaffold Name=DefaultConnection Microsoft.EntityFrameworkCore.SqlServer -o Models -d"

---
+++ Designing API To Ready To Go Database/Data/MarketContext.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Designing_API_To_Ready_To_Go_Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Designing_API_To_Ready_To_Go_Database.Data;
+
+public partial class MarketContext : DbContext
+{
+    public MarketContext()
+    {
+    }
+
+    public MarketContext(DbContextOptions<MarketContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Musteriler> Musterilers { get; set; }
+
+    public virtual DbSet<SiparisDetay> SiparisDetays { get; set; }
+
+    public virtual DbSet<Siparisler> Siparislers { get; set; }
+
+    public virtual DbSet<Urunler> Urunlers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Musteriler>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Musteril__3214EC079FFD8624");
+
+            entity.ToTable("Musteriler");
+
+            entity.Property(e => e.Id)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Email)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Isim)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.KullaniciAdi)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Kullanici Adi");
+            entity.Property(e => e.ParolaH)
+                .HasMaxLength(267)
+                .IsUnicode(false);
+            entity.Property(e => e.Soyisim)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<SiparisDetay>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("SiparisDetay");
+
+            entity.HasOne(d => d.Siparis).WithMany()
+                .HasForeignKey(d => d.SiparisId)
+                .HasConstraintName("FK__SiparisDe__Sipar__3F466844");
+
+            entity.HasOne(d => d.Urun).WithMany()
+                .HasForeignKey(d => d.UrunId)
+                .HasConstraintName("FK__SiparisDe__UrunI__403A8C7D");
+        });
+
+        modelBuilder.Entity<Siparisler>(entity =>
+        {
+            entity.HasKey(e => e.SiparisId).HasName("PK__Siparisl__C3F03BFD47E9E7AA");
+
+            entity.ToTable("Siparisler");
+
+            entity.Property(e => e.SiparisId).ValueGeneratedNever();
+            entity.Property(e => e.MusteriId)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Musteri).WithMany(p => p.Siparislers)
+                .HasForeignKey(d => d.MusteriId)
+                .HasConstraintName("FK__Siparisle__Muste__3D5E1FD2");
+        });
+
+        modelBuilder.Entity<Urunler>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Urunler__3214EC07F4201B4A");
+
+            entity.ToTable("Urunler");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.DepoMiktari).HasColumnName("Depo Miktari");
+            entity.Property(e => e.Isim)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Kategori)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Designing API To Ready To Go Database/Models/Urunler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Designing_API_To_Ready_To_Go_Database.Models;
+
+[Table("Urunler")]
+public partial class Urunler
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(30)]
+    [Unicode(false)]
+    public string? Isim { get; set; }
+
+    [StringLength(30)]
+    [Unicode(false)]
+    public string? Kategori { get; set; }
+
+    public int? Fiyat { get; set; }
+
+    [Column("Depo Miktari")]
+    public int? DepoMiktari { get; set; }
+}
