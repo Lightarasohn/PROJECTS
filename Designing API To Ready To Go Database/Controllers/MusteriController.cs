5259d68a05c77c99e4fa3b3070c476b5107b4816
Onur Demir <onurdemir1771@gmail.com>
2025-03-21 12:10:34 +0300

    Musteri Controller tamamlandı

--- Designing API To Ready To Go Database/Controllers/MusteriController.cs
+++ Designing API To Ready To Go Database/Controllers/MusteriController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Designing_API_To_Ready_To_Go_Database.DTOs.MusteriDTOs;
 using Designing_API_To_Ready_To_Go_Database.Interfaces;
+using Designing_API_To_Ready_To_Go_Database.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Designing_API_To_Ready_To_Go_Database.Controllers
@@ -15,6 +17,62 @@ namespace Designing_API_To_Ready_To_Go_Database.Controllers
         public MusteriController(IMusteriRepository musteriRepo)
         {
             _musteriRepo = musteriRepo;
-        }   
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMusteriler()
+        {
+            var musteriler = await _musteriRepo.GetAllMusterilerAsync();
+            return Ok(musteriler);
+        }
+
+        [HttpGet("get/id/{id}")]
+        public async Task<IActionResult> GetMusteriById([FromRoute] string id)
+        {
+            var musteri = await _musteriRepo.GetMusteriByIdAsync(id);
+
+            if(musteri == null)
+                return NotFound("Musteri Bulunamadi");
+
+            return Ok(musteri);
+        }
+
+        [HttpGet("get/email/{mail}")]
+        public async Task<IActionResult> GetMusteriByEmail([FromRoute] string email)
+        {
+            var musteri = await _musteriRepo.GetMusteriByEmailAsync(email);
+
+            if(musteri == null)
+                return NotFound("Musteri bulunamdi");
+            
+            return Ok(musteri);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMusteri([FromRoute] string id)
+        {
+            var musteri = await _musteriRepo.DeleteMusteriByIdAsync(id);
+
+            if(musteri == null)
+                return NotFound("Musteri Bulunamadi");
+
+            return Ok(musteri);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMusteri([FromBody] MusteriCreateDto dto)
+        {
+            var createdMusteri = await _musteriRepo.CreateMusteriAsync(dto);
+
+            if(createdMusteri.IsUsernameExist)
+                return BadRequest("Kullanici adi kullanimda");
+
+            if(createdMusteri.IsEmailExist)
+                return BadRequest("Email kullanimda");
+
+            var musteriTokenDto = createdMusteri.ToMusteriTokenDto();
+            
+            return CreatedAtAction(nameof(CreateMusteri), new {musteriTokenDto.Id}, musteriTokenDto);
+        }
     }
 }
